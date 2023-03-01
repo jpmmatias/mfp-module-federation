bbf44bb7025fd8a125b33fa0987d9c18722270b7
jpmmatias <jpmmatias2018@outlook.com>
2023-03-01 10:56:33 -0300

    created marketing microfront

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#marketing-root");
+  mount(el);
+}
+
+export { mount };
---
+++ marketing/src/index.js
@@ -0,0 +1,2 @@
+import { mount } from "./bootstrap";
+mount(document.querySelector("#marketing-root"));
