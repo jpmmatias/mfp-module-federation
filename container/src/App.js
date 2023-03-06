d663713a99d55ab9a72db9d9337de00c28738e59
jpmmatias <jpmmatias2018@outlook.com>
2023-03-06 15:05:34 -0300

    chage for te github action activate

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 const App = () => {
-  return <div>Hi there!
-
-    <MarketingApp/>
-  </div>;
+  return (
+    <div>
+      Hi there!!
+      <MarketingApp />
+    </div>
+  );
 };
 
 export default App;
